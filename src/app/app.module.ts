f5509a6dded1c25d7a5577890332754450af65c2
ledavid0520 <ledavis275@gmail.com>
2021-05-28 18:27:52 -0500

    COMMIT: i18n implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import { Action, MetaReducer, StoreModule } from '@ngrx/store';
 import { StoreDevtools, StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
 
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService
+} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { reduceProvider, REDUCERS_TOKEN } from './store';
@@ -17,9 +24,14 @@ import { CategoryComponent } from './pages/category/category.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { PaymentConfirmationComponent } from './pages/payment-confirmation/payment-confirmation.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 const metaReducers: MetaReducer<Object, Action>[] = []
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -37,10 +49,25 @@ const metaReducers: MetaReducer<Object, Action>[] = []
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot(REDUCERS_TOKEN, {metaReducers}),
-    !environment.production && StoreDevtoolsModule.instrument()
+    FormsModule,
+    StoreModule.forRoot(REDUCERS_TOKEN, { metaReducers }),
+    !environment.production && StoreDevtoolsModule.instrument(),
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   providers: [reduceProvider],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(translate: TranslateService) {
+    translate.addLangs(['es-co', 'en']);
+    translate.setDefaultLang('es-co');
+    translate.use('es-co');
+  }
+}
