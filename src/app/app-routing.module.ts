5d6cfd95010e59224c3ddffb6c2467cfe95b944a
rivera-miguel@hotmail.com <rivera-miguel@hotmail.com>
2021-05-28 18:22:09 -0500

    pagina detail del producto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { RoutesApp } from './enums/routes.enum';
 import { CategoryComponent } from './pages/category/category.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
 const routes: Routes = [
   {path:RoutesApp.HOME, component: HomeComponent},
   {path:RoutesApp.CATEGORY, component: CategoryComponent},
+  {path:RoutesApp.PRODUCT, component: ProductDetailComponent},
   {path:'', redirectTo: '/home', pathMatch: 'full'}
 ];
 
